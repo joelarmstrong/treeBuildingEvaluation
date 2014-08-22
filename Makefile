aa25a7fb80d524c2d2c285bd8707f43e281afef4
Joel Armstrong <jcarmstr@ucsc.edu>
2014-08-22 14:12:59 -0700

    Add the column tree in a fasta comment

--- Makefile
+++ Makefile
@@ -4,5 +4,6 @@ all:
 	python rebaseFastaCoordinates.py pastaIteration1_origNames.pruned.fa origNamesToFinalNames > pastaIteration1_finalNames.fa
 	python renameNewick.py origNamesToFinalNames pastaIteration1_origNames.nh > pastaIteration1_finalNames.nh
 	python getSpimapGene2Species.py pastaIteration1_finalNames.fa >pastaIteration1_finalNames.smap
+	gcc -std=c99 -O0 -g -o reconcile reconcile.c -I /cluster/home/jcarmstr/progressiveCactus/submodules/sonLib/C/inc/ -I  /cluster/home/jcarmstr/progressiveCactus/submodules/pinchesAndCacti/inc /cluster/home/jcarmstr/progressiveCactus/submodules/sonLib/lib/stPinchesAndCacti.a /cluster/home/jcarmstr/progressiveCactus/submodules/sonLib/lib/*.a -lm -lstdc++
 	./reconcile pastaIteration1_finalNames.smap $(shell cat pastaIteration1_finalNames.nh) $(shell cat speciesTree.nh) 1 > pastaIteration1_finalNames.reconciled.nh
 	 python mafFromTreeAndRebasedFasta.py pastaIteration1_finalNames.reconciled.nh pastaIteration1_finalNames.fa > pastaIteration1_toCompare.maf
--- getRegionAroundSampledColumn.cpp
+++ getRegionAroundSampledColumn.cpp
@@ -1,5 +1,6 @@
 // Samples a column and extracts a FASTA of the region surrounding
 // every column entry.
+#include <time.h>
 #include "hal.h"
 #include "sonLib.h"
 #include "bioioC.h"
@@ -16,6 +17,120 @@ static CLParserPtr initParser()
   optionsParser->addOption("refPos", "position (only valid if also using --sequence)", -1);
   optionsParser->addOption("width", "width of the region around the sampled column to extract, default = 1000", 1000);
   return optionsParser;
+}
+
+// Adapted from the hal2maf block-tree building code.
+// Could put this general version into the column iterator interface.
+
+// Builds a "gene"-tree node and labels it properly.
+static stTree *getTreeNode(SegmentIteratorConstPtr segIt)
+{
+  // Make sure the segment is sliced to only 1 base.
+  assert(segIt->getStartPosition() == segIt->getEndPosition());
+  stTree *ret = stTree_construct();
+  const Genome *genome = segIt->getGenome();
+  const Sequence *seq = genome->getSequenceBySite(segIt->getStartPosition());
+  stringstream ss;
+  ss << segIt->getGenome()->getName() << "." << seq->getName() << "|" << segIt->getStartPosition() - seq->getStartPosition();
+  stTree_setLabel(ret, stString_copy(ss.str().c_str()));
+
+  return ret;
+}
+
+// Recursive part of buildTree
+// tree parameter represents node corresponding to the genome with
+// bottom segment botIt
+static void buildTreeR(BottomSegmentIteratorConstPtr botIt, stTree *tree)
+{
+  const Genome *genome = botIt->getGenome();
+
+  // attach a node and recurse for each of this segment's children
+  // (and paralogous segments)
+  for (hal_size_t i = 0; i < botIt->getNumChildren(); i++) {
+    if (botIt->hasChild(i)) {
+      const Genome *child = genome->getChild(i);
+      TopSegmentIteratorConstPtr topIt = child->getTopSegmentIterator();
+      topIt->toChild(botIt, i);
+      stTree *canonicalParalog = getTreeNode(topIt);
+      stTree_setParent(canonicalParalog, tree);
+      if (topIt->hasParseDown()) {
+        BottomSegmentIteratorConstPtr childBotIt = child->getBottomSegmentIterator();
+        childBotIt->toParseDown(topIt);
+        buildTreeR(childBotIt, canonicalParalog);
+      }
+      // Traverse the paralogous segments cycle and add those segments as well
+      if (topIt->hasNextParalogy()) {
+        topIt->toNextParalogy();
+        while(!topIt->isCanonicalParalog()) {
+          stTree *paralog = getTreeNode(topIt);
+          stTree_setParent(paralog, tree);
+          if(topIt->hasParseDown()) {
+            BottomSegmentIteratorConstPtr childBotIt = child->getBottomSegmentIterator();
+            childBotIt->toParseDown(topIt);
+            buildTreeR(childBotIt, paralog);
+          }
+          topIt->toNextParalogy();
+        }
+      }
+    }
+  }
+}
+
+// Build a gene-tree from a column iterator.
+static stTree *buildTree(ColumnIteratorConstPtr colIt)
+{
+  // Get any base from the column to begin building the tree
+  const ColumnIterator::ColumnMap *colMap = colIt->getColumnMap();
+  ColumnIterator::ColumnMap::const_iterator colMapIt = colMap->begin();
+  const Sequence *sequence = NULL;
+  hal_index_t index = NULL_INDEX;
+  while (colMapIt != colMap->end()) {
+    if (!colMapIt->second->empty()) {
+      // Found a non-empty column map entry, just take the index and
+      // sequence of the first base found
+      sequence = colMapIt->first;
+      index = colMapIt->second->at(0)->getArrayIndex();
+      break;
+    }
+    colMapIt++;
+  }
+  assert(sequence != NULL && index != NULL_INDEX);
+  const Genome *genome = sequence->getGenome();
+
+  // Get the bottom segment that is the common ancestor of all entries
+  TopSegmentIteratorConstPtr topIt = genome->getTopSegmentIterator();
+  BottomSegmentIteratorConstPtr botIt;
+  if (genome->getNumTopSegments() == 0) {
+    // The reference is the root genome.
+    botIt = genome->getBottomSegmentIterator();
+    botIt->toSite(index);
+  } else {
+    // Keep heading up the tree until we hit the root segment.
+    topIt->toSite(index);
+    while (topIt->hasParent()) {
+      const Genome *parent = topIt->getGenome()->getParent();
+      botIt = parent->getBottomSegmentIterator();
+      botIt->toParent(topIt);
+      if(parent->getParent() == NULL || !botIt->hasParseUp()) {
+        // Reached root genome
+        break;
+      }
+      topIt = parent->getTopSegmentIterator();
+      topIt->toParseUp(botIt);
+    }
+  }
+
+  stTree *tree = NULL;
+  if(topIt->hasParent() == false && topIt->getGenome() == genome && genome->getNumBottomSegments() == 0) {
+    // Handle insertions in leaves. botIt doesn't point anywhere since
+    // there are no bottom segments.
+    tree = getTreeNode(topIt);
+  } else {
+    tree = getTreeNode(botIt);
+    buildTreeR(botIt, tree);
+  }
+  assert(tree != NULL);
+  return tree;
 }
 
 int main(int argc, char *argv[])
@@ -37,6 +152,8 @@ int main(int argc, char *argv[])
     return 1;
   }
 
+  st_randomSeed(time(NULL));
+
   AlignmentConstPtr alignment = openHalAlignment(halPath, optParser);
   const Genome *genome = alignment->openGenome(genomeName);
   if (genome == NULL) {
@@ -53,14 +170,19 @@ int main(int argc, char *argv[])
     refSequence = genome->getSequence(refSequenceName);
     hal_size_t sequenceLen = refSequence->getSequenceLength();
     refPos = st_randomInt64(0, sequenceLen - 1);
+    refPos += refSequence->getStartPosition();
   } else {
     refSequence = genome->getSequence(refSequenceName);
+    refPos += refSequence->getStartPosition();
   }
-  refPos += refSequence->getStartPosition();
 
   char *outputSeq = (char *) malloc((width*2 + 2) * sizeof(char));
 
   ColumnIteratorConstPtr colIt = genome->getColumnIterator(NULL, 0, refPos, NULL_INDEX, false, true);
+
+  // Print out the tree underlying this column as a FASTA comment.
+  cout << "#" << stTree_getNewickTreeString(buildTree(colIt)) << endl;
+
   const ColumnIterator::ColumnMap *cols = colIt->getColumnMap();
   ColumnIterator::ColumnMap::const_iterator colMapIt;
   for (colMapIt = cols->begin(); colMapIt != cols->end(); colMapIt++) {
@@ -97,8 +219,11 @@ int main(int argc, char *argv[])
       outputSeq[size] = '\0';
 
       stringstream header;
-      header << genome->getName() << "." << seq->getName() << ":" << midpoint;
+      header << seq->getGenome()->getName() << "." << seq->getName() << "|" << midpoint;
       fastaWrite(outputSeq, (char *) header.str().c_str(), stdout);
     }
   }
+
+  // Intentionally not dealing with memory leaks for this very
+  // short-lived process.
 }
